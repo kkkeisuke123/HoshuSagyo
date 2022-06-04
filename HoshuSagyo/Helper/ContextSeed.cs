a502c0c20454abbe69569ce4388a28da1d5cee6e
AK <suzuki.k.1250@gmail.com>
2022-06-04 15:41:00 +0900

    ロール情報の初期登録処理を追加した

---
+++ HoshuSagyo/Helper/ContextSeed.cs
@@ -0,0 +1,40 @@
+﻿using HoshuSagyo.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+namespace HoshuSagyo.Helper
+{
+    public static class ContextSeed
+    {
+        public static async Task SeedRolesAsync(
+            UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Roles
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Shirei.ToString()));
+            await roleManager.CreateAsync(new IdentityRole(Enums.Roles.Genchi.ToString()));
+        }
+
+        public static async Task SeedSuperAdminAsync(
+            UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            //Seed Default User
+            var defaultUser = new ApplicationUser
+            {
+                UserName = "TokyoShirei@gmail.com",
+                Email = "TokyoShirei@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumberConfirmed = true,
+                Kankatsu = 1
+            };
+            if (userManager.Users.All(u => u.Id != defaultUser.Id))
+            {
+                var user = await userManager.FindByEmailAsync(defaultUser.Email);
+                if (user == null)
+                {
+                    await userManager.CreateAsync(defaultUser, "123Pa$$word.");
+                    await userManager.AddToRoleAsync(defaultUser, Enums.Roles.Shirei.ToString());
+                }
+            }
+        }
+    }
+}
