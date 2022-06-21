42c6bfac11e258f00a4669f78db2659cfa39b0b3
AK <suzuki.k.1250@gmail.com>
2022-06-21 17:59:19 +0900

    作業計画登録処理を作成した

---
+++ HoshuSagyo/Controllers/SagyoKeikakuController.cs
@@ -0,0 +1,63 @@
+﻿using HoshuSagyo.Data;
+using HoshuSagyo.Models.Components;
+using HoshuSagyo.Models.InputModels;
+using HoshuSagyo.Models.Transactions;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HoshuSagyo.Controllers
+{
+    public class SagyoKeikakuController : Controller
+    {
+        private readonly HoshuSagyoDbContext _hoshuSagyoDbContext;
+
+        public SagyoKeikakuController(HoshuSagyoDbContext hoshuSagyoDbContext)
+        {
+            _hoshuSagyoDbContext = hoshuSagyoDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(GetGamenItems());
+        }
+
+        private SagyoKeikakuTorokuGamen GetGamenItems()
+        {
+            var gamenItems = new SagyoKeikakuTorokuGamen();
+
+            // ドロップダウンリストの項目を用意
+            gamenItems.KeitoList = _hoshuSagyoDbContext.M_Keito.Select(x => new Itemlist { Value = x.Keito, Text = x.KeitoName }).ToList();
+            gamenItems.KankatsuList = _hoshuSagyoDbContext.M_Kankatsu.Select(x => new Itemlist { Value = x.Kankatsu, Text = x.KankatsuName }).ToList();
+            gamenItems.SagyoShubetsuList = _hoshuSagyoDbContext.M_SagyoShubetsu.Select(x => new Itemlist { Value = x.SagyoShubetsu, Text = x.SagyoShubetsuName }).ToList();
+            gamenItems.SagyoBashoList = _hoshuSagyoDbContext.M_SagyoBasho.Select(x => new Itemlist { Value = x.SagyoBasho, Text = x.SagyoBashoName }).ToList();
+
+            return gamenItems;
+        }
+
+        [HttpPost]
+        public IActionResult DoToroku([FromForm] SagyoKeikakuTorokuGamen inputValue)
+        {
+            // 登録処理を実行
+            var sagyoKeikakuModel = new SagyoKeikakuModel
+            {
+                SagyoKaishiBi = inputValue.SagyoKaishiBi.ToShortDateString(),
+                SagyoKaishiJikan = inputValue.SagyoKaishiJikan.ToShortTimeString(),
+                SagyoShuryoBi = inputValue.SagyoShuryoBi.ToShortDateString(),
+                SagyoShuryoJikan = inputValue.SagyoShuryoJikan.ToShortTimeString(),
+                Keito = inputValue.Keito,
+                Kankatsu = inputValue.Kankatsu,
+                SagyoShubetsu = inputValue.SagyoShubetsu,
+                SagyoBasho = inputValue.SagyoBasho,
+                SagyoNaiyo = inputValue.SagyoNaiyo,
+                JishaSekininshaMei = inputValue.JishaSekininshaMei,
+                JishaSagyoinSu = inputValue.JishaSagyoinSu,
+                KyoryokuGaishaSekininshaMei = inputValue.KyoryokuGaishaSekininshaMei,
+                KyoryokuGaishaSagyoinSu = inputValue.KyoryokuGaishaSagyoinSu
+            };
+
+            _hoshuSagyoDbContext.Add(sagyoKeikakuModel);
+            _hoshuSagyoDbContext.SaveChanges();
+
+            return View("Result", sagyoKeikakuModel);
+        }
+    }
+}
---
+++ HoshuSagyo/Models/InputModels/SagyoKeikakuTorokuGamen.cs
@@ -0,0 +1,73 @@
+﻿using HoshuSagyo.Models.Components;
+using System.ComponentModel.DataAnnotations;
+
+namespace HoshuSagyo.Models.InputModels
+{
+    public class SagyoKeikakuTorokuGamen
+    {
+        public List<Itemlist> KeitoList;
+        public List<Itemlist> KankatsuList;
+        public List<Itemlist> SagyoShubetsuList;
+        public List<Itemlist> SagyoBashoList;
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "作業開始日")]
+        public DateTime SagyoKaishiBi { get; set; }
+        
+        [Required]
+        [DataType(DataType.Time)]
+        [Display(Name = "作業開始時間")]
+        public DateTime SagyoKaishiJikan { get; set; }
+
+        [Required]
+        [DataType(DataType.Date)]
+        [Display(Name = "作業終了日")]
+        public DateTime SagyoShuryoBi { get; set; }
+
+        [Required]
+        [DataType(DataType.Time)]
+        [Display(Name = "作業終了時間")]
+        public DateTime SagyoShuryoJikan { get; set; }
+
+        [Required]
+        [Display(Name = "系統")]
+        public int Keito { get; set; }
+
+        [Required]
+        [Display(Name = "管轄")]
+        public int Kankatsu { get; set; }
+
+        [Required]
+        [Display(Name = "作業種別")]
+        public int SagyoShubetsu { get; set; }
+
+        [Required]
+        [Display(Name = "作業場所")]
+        public int SagyoBasho { get; set; }
+
+        [Display(Name = "作業番号")]
+        public int? SagyoBango { get; set; }
+
+        [Required]
+        [MaxLength(90)]
+        [Display(Name = "作業内容")]
+        public string SagyoNaiyo { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "自社責任者名")]
+        public string? JishaSekininshaMei { get; set; }
+
+        [Range(0, 999)]
+        [Display(Name = "自社作業員数")]
+        public int? JishaSagyoinSu { get; set; }
+
+        [MaxLength(20)]
+        [Display(Name = "協力会社責任者名")]
+        public string? KyoryokuGaishaSekininshaMei { get; set; }
+
+        [Range(0, 999)]
+        [Display(Name = "協力会社作業員数")]
+        public int? KyoryokuGaishaSagyoinSu { get; set; }
+    }
+}
