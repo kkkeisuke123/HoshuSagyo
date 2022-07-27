3e76dbc29e72e8cd8bb7343db8e7b56fc94440cf
AK <suzuki.k.1250@gmail.com>
2022-07-27 14:24:59 +0900

    音声応答設定画面の入力チェック（クライアント側）を追加した

--- HoshuSagyo/Models/DisplayModels/OnseiOtoDisplayContent.cs
+++ HoshuSagyo/Models/DisplayModels/OnseiOtoDisplayContent.cs
@@ -10,19 +10,15 @@ namespace HoshuSagyo.Models.DisplayModels
         public List<Itemlist> OnseiOtoShosaiList;
 
         // 画面項目
-        [Required(ErrorMessage = "音声応答種別を入力してください")]
         [Display(Name = "音声応答種別")]
         public int SagyoKaishiOnseiOtoShubetsu { get; set; }
 
-        [Required(ErrorMessage = "音声応答メッセージを入力してください")]
         [Display(Name = "音声応答メッセージ")]
         public int SagyoKaishiOnseiOtoShosaiId { get; set; }
 
-        [Required(ErrorMessage = "音声応答種別を入力してください")]
         [Display(Name = "音声応答種別")]
         public int SagyoShuryoOnseiOtoShubetsu { get; set; }
 
-        [Required(ErrorMessage = "音声応答メッセージを入力してください")]
         [Display(Name = "音声応答メッセージ")]
         public int SagyoShuryoOnseiOtoShosaiId { get; set; }
     }
--- HoshuSagyo/wwwroot/js/onseiOto.js
+++ HoshuSagyo/wwwroot/js/onseiOto.js
@@ -1,9 +1,9 @@
 ﻿
-// 音声応答の応答種別（ドロップダウン）が選択されたとき、
-// その種別に対応するメッセージの一覧を返します
 $(document).ready(function () {
-    $("#kaishiShubetsuList").change(function () {
 
+    // 作業開始：音声応答種別ドロップダウン選択項目を変更した時の処理
+    $("#kaishiShubetsuList").change(function () {
+        // 選択された種別に対応するメッセージの一覧を取得します
         $.ajax({
             type: "Post",
             url: "/AjaxUtility/GetOnseiOtoShosai",
@@ -16,10 +16,21 @@ $(document).ready(function () {
             })
             $("#kaishiMessageList").html(items);
         });
+
+        // エラーメッセージをクリア
+        $("#kaishi-shubetsu-error").text("");
+        $("#kaishi-message-error").text("");
     });
 
-    $("#shuryoShubetsuList").change(function () {
+    // 作業開始：音声応答メッセージドロップダウン選択項目を変更した時の処理
+    $("#kaishiMessageList").change(function () {
+         // エラーメッセージをクリア
+        $("#kaishi-message-error").text("");
+    });
 
+    // 作業終了：音声応答種別ドロップダウン選択項目を変更した時の処理
+    $("#shuryoShubetsuList").change(function () {
+        // 選択された種別に対応するメッセージの一覧を取得します
         $.ajax({
             type: "Post",
             url: "/AjaxUtility/GetOnseiOtoShosai",
@@ -32,5 +43,64 @@ $(document).ready(function () {
             })
             $("#shuryoMessageList").html(items);
         });
+
+        // エラーメッセージをクリア
+        $("#shuryo-shubetsu-error").text("");
+        $("#shuryo-message-error").text("");
     });
+
+    // 作業終了：音声応答メッセージドロップダウン選択項目を変更した時の処理
+    $("#shuryoMessageList").change(function () {
+        // エラーメッセージをクリア
+        $("#shuryo-message-error").text("");
+    });
+
+    // 設定ボタン押下時の処理
+    $("#btn-setting").click(function () {
+
+        let inputOkFlg = true;
+
+        // 入力状態をチェック
+        if ($("#kaishiShubetsuList").val() === "") {
+            $("#kaishi-shubetsu-error").text("音声応答種別を選択してください");
+            inputOkFlg = false;
+        }
+        if ($("#kaishiMessageList").val() === "") {
+            $("#kaishi-message-error").text("音声応答メッセージを選択してください");
+            inputOkFlg = false;
+        }
+        if ($("#shuryoShubetsuList").val() === "") {
+            $("#shuryo-shubetsu-error").text("音声応答種別を選択してください");
+            inputOkFlg = false;
+        }
+        if ($("#shuryoMessageList").val() === "") {
+            $("#shuryo-message-error").text("音声応答メッセージを選択してください");
+            inputOkFlg = false;
+        }
+
+        if (inputOkFlg === true) {
+            // モーダルを閉じる
+            $("#btn-close").click();
+        }
+    });
+
+    // 閉じるボタン押下時の処理
+    $("#btn-close").click(function () {
+        // エラーメッセージを全てクリア
+        clearAllErrorMassage();
+    });
+
+    // 「×」ボタン押下時の処理
+    $(".btn-close").click(function () {
+        // エラーメッセージを全てクリア
+        clearAllErrorMassage();
+    });
+
+    // 入力チェックエラー時のメッセージをクリアする
+    function clearAllErrorMassage() {
+        $("#kaishi-shubetsu-error").text("");
+        $("#kaishi-message-error").text("");
+        $("#shuryo-shubetsu-error").text("");
+        $("#shuryo-message-error").text("");
+    }
 }); 
