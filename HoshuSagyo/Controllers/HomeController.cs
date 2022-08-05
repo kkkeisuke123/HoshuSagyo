46ffaa98f9d79940e39697513a581631c75e0e1d
AK <suzuki.k.1250@gmail.com>
2022-08-05 09:13:21 +0900

    アプリ説明用のページを追加

--- HoshuSagyo/Controllers/HomeController.cs
+++ HoshuSagyo/Controllers/HomeController.cs
@@ -25,5 +25,49 @@ namespace HoshuSagyo.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        /// <summary>
+        /// アプリの概要
+        /// </summary>
+        /// <returns>アプリの概要ページ</returns>
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 画面遷移図
+        /// </summary>
+        /// <returns>画面遷移図ページ</returns>
+        public IActionResult TransitionDiagram()
+        {
+            return View();
+        }
+
+        /// <summary>
+        /// 画面の説明
+        /// </summary>
+        /// <returns>画面の説明ページ</returns>
+        public IActionResult ScreenExplanation(int? id)
+        {
+            if (id is null)
+            {
+                return View();
+            }
+            else
+            {
+                string pageId = ((int)id).ToString("D2");
+                return View($"Gamen/{pageId}");
+            }
+        }
+
+        /// <summary>
+        /// 操作方法
+        /// </summary>
+        /// <returns>操作方法ページ</returns>
+        public IActionResult OperationMethod()
+        {
+            return View();
+        }
     }
 }
---
+++ HoshuSagyo/wwwroot/js/about.js
@@ -0,0 +1,18 @@
+﻿$(document).ready(function () {
+    let pathname = $(location).attr("pathname");
+    let actionname = pathname.slice(pathname.lastIndexOf("/") + 1);
+    console.log(actionname);
+    if (actionname === "About") {
+        $("#about").addClass("active");
+    }
+    else if (actionname === "TransitionDiagram") {
+        $("#transitionDiagram").addClass("active");
+
+    }
+    else if (actionname === "ScreenExplanation") {
+        $("#screenExplanation").addClass("active");
+    }
+    else if (actionname === "OperationMethod") {
+        $("#operationMethod").addClass("active");
+    }
+});
