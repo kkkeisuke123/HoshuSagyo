f6a396646b4472de1877a3f394907de00ccba341
AK <suzuki.k.1250@gmail.com>
2022-08-07 16:44:08 +0900

    プロファイル画面にユーザーの管轄名を表示した

--- HoshuSagyo/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
+++ HoshuSagyo/Areas/Identity/Pages/Account/Manage/Index.cshtml.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 #nullable disable
 
+using HoshuSagyo.Data;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -12,11 +13,13 @@ namespace HoshuSagyo.Areas.Identity.Pages.Account.Manage
     public class IndexModel : PageModel
     {
         private readonly UserManager<IdentityUser> _userManager;
-        
+        private readonly HoshuSagyoDbContext _hoshuSagyoDbContext;
+
         public IndexModel(
-            UserManager<IdentityUser> userManager)
+            UserManager<IdentityUser> userManager, HoshuSagyoDbContext hoshuSagyoDbContext)
         {
             _userManager = userManager;
+            _hoshuSagyoDbContext = hoshuSagyoDbContext;
         }
 
         /// <summary>
@@ -25,6 +28,9 @@ namespace HoshuSagyo.Areas.Identity.Pages.Account.Manage
         /// </summary>
         [Display(Name ="ユーザー名")]
         public string Username { get; set; }
+
+        [Display(Name ="管轄名")]
+        public string KankatsuName { get; set; }
 
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
@@ -36,8 +42,13 @@ namespace HoshuSagyo.Areas.Identity.Pages.Account.Manage
         private async Task LoadAsync(IdentityUser user)
         {
             var userName = await _userManager.GetUserNameAsync(user);
-            
             Username = userName;
+
+            // クレームから管轄を取得
+            int kankatsu = int.Parse(User.Claims.FirstOrDefault(x => x.Type == "Kankatsu").Value);
+
+            // DBから管轄名を取得
+            KankatsuName = _hoshuSagyoDbContext.M_Kankatsu.FirstOrDefault(x => x.Kankatsu == kankatsu).KankatsuName;
         }
 
         public async Task<IActionResult> OnGetAsync()
